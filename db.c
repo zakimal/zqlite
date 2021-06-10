ae8b68c18b9ff8cd33fa0ff48b2634f2415de480
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2021-06-10 02:26:55 +0000

    modify do_meta_command to use db_close()

--- db.c
+++ db.c
@@ -119,7 +119,7 @@ MetaCommandResult do_meta_command(InputBuffer *input_buffer, Table *table)
     if (strcmp(input_buffer->buffer, ".exit") == 0)
     {
         close_input_buffer(input_buffer);
-        free_table(table);
+        db_close(table);
         exit(EXIT_SUCCESS);
     }
     else
