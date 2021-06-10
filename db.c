b6219223c032a9d25f859363a1cee2d6a2770efd
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2021-06-10 03:31:00 +0000

    use strncpy

--- db.c
+++ db.c
@@ -252,8 +252,8 @@ void print_row(Row *row)
 void serialize_row(Row *source, void *destination)
 {
     memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
-    memcpy(destination + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
-    memcpy(destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
+    strncpy(destination + USERNAME_OFFSET, source->username, USERNAME_SIZE);
+    strncpy(destination + EMAIL_OFFSET, source->email, EMAIL_SIZE);
 }
 
 void deserialize_row(void *source, Row *destination)
