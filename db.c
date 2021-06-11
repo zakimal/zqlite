a77f8de12b2da109a18933ba87bf60188c6d4416
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2021-06-11 12:31:38 +0000

    use cursor abstraction in insert

--- db.c
+++ db.c
@@ -199,9 +199,12 @@ ExecuteResult execute_insert(Statement *statement, Table *table)
     }
 
     Row *row_to_insert = &(statement->row_to_insert);
+    Cursor *cursor = table_end(table);
 
-    serialize_row(row_to_insert, row_slot(table, table->num_rows));
+    serialize_row(row_to_insert, cursor_value(cursor));
     table->num_rows += 1;
+
+    free(cursor);
 
     return EXECUTE_SUCCESS;
 }
