edd5395b0da995bbd8787fd29682a298de0cb51c
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2021-06-14 04:40:46 +0000

    print_leaf_node -> print_tree

--- db.c
+++ db.c
@@ -190,7 +190,8 @@ NodeType get_node_type(void *node);
 bool is_node_root(void *node);
 void set_node_type(void *node, NodeType type);
 void print_constants();
-void print_leaf_node(void *node);
+void indent(uint32_t level);
+void print_tree(Pager *pager, uint32_t page_num, uint32_t indentation_level);
 
 InputBuffer *new_input_buffer()
 {
@@ -845,14 +846,46 @@ void print_constants()
 }
 
 // for debug
-void print_leaf_node(void *node)
+void indent(uint32_t level)
 {
-    uint32_t num_cells = *leaf_node_num_cells(node);
-    printf("leaf (size %d)\n", num_cells);
-    for (uint32_t i = 0; i < num_cells; i++)
+    for (uint32_t i = 0; i < level; i++)
     {
-        uint32_t key = *leaf_node_key(node, i);
-        printf("  - key=%d: value=%d\n", i, key);
+        printf("    ");
+    }
+}
+
+void print_tree(Pager *pager, uint32_t page_num, uint32_t indentation_level)
+{
+    void *node = get_page(pager, page_num);
+    uint32_t num_keys, child;
+
+    switch (get_node_type(node))
+    {
+    case NODE_LEAF:
+        num_keys = *leaf_node_num_cells(node);
+        indent(indentation_level);
+        printf("- leaf (size %d)\n", num_keys);
+        for (uint32_t i = 0; i < num_keys; i++)
+        {
+            indent(indentation_level + 1);
+            printf("- %d\n", *leaf_node_key(node, i));
+        }
+        break;
+    case NODE_INTERNAL:
+        num_keys = *internal_node_num_keys(node);
+        indent(indentation_level);
+        printf("- internal (size %d)\n", num_keys);
+        for (uint32_t i = 0; i < num_keys; i++)
+        {
+            child = *internal_node_child(node, i);
+            print_tree(pager, child, indentation_level + 1);
+
+            indent(indentation_level + 1);
+            printf("- key %d\n", *internal_node_key(node, i));
+        }
+        child = *internal_node_right_child(node);
+        print_tree(pager, child, indentation_level + 1);
+        break;
     }
 }
 
