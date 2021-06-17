9c48e5dfcd47281599bd785042e80a99f0e1db6e
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2021-06-17 03:40:01 +0000

    fix

--- db.c
+++ db.c
@@ -180,7 +180,7 @@ uint32_t *internal_node_cell(void *node, uint32_t cell_num);
 uint32_t *internal_node_child(void *node, uint32_t child_num);
 uint32_t *internal_node_key(void *node, uint32_t key_num);
 void update_internal_node_key(void *node, uint32_t old_key, uint32_t new_key);
-uint32_t internal_node_find_child(void *node, uint32_t page_num, uint32_t key);
+uint32_t internal_node_find_child(void *node, uint32_t key);
 Cursor *internal_node_find(Table *table, uint32_t page_num, uint32_t key);
 void internal_node_insert(Table *table, uint32_t parent_page_num, uint32_t child_page_num);
 uint32_t get_node_max_key(void *node);
@@ -677,7 +677,7 @@ void update_internal_node_key(void *node, uint32_t old_key, uint32_t new_key)
     *internal_node_key(node, old_child_index) = new_key;
 }
 
-uint32_t internal_node_find_child(void *node, uint32_t page_num, uint32_t key)
+uint32_t internal_node_find_child(void *node, uint32_t key)
 {
     // Return the index of the child which should contain the given key.
 
@@ -872,7 +872,7 @@ void leaf_node_split_and_insert(Cursor *cursor, uint32_t key, Row *value)
     {
         uint32_t parent_page_num = *node_parent(old_node);
         uint32_t new_max = get_node_max_key(old_node);
-        void *parent = get_page(cursor->table, parent_page_num, new_page_num);
+        void *parent = get_page(cursor->table->pager, new_page_num);
 
         update_internal_node_key(parent, old_max, new_max);
         internal_node_insert(cursor->table, parent_page_num, new_page_num);
